d4245f34780fffbec7488c784953ffb4ad8d506c
xuzho <xuzho@microsoft.com>
2016-09-30 10:58:13 +0800

    fix ps

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -10,7 +10,7 @@ $docfx = "docfx\"
 $docfxExeUrl = "https://github.com/dotnet/docfx/releases/download/v2.1.0-cli-alpha/docfx.cli.zip"
 
 # download docfx from github.com
-Invoke-WebRequest $docfxExeUrl -OutFile $docfxZip
+Invoke-WebRequest $docfxExeUrl -OutFile $rootFolder\$docfxZip
 
 # unzip docfx.zip
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -21,7 +21,7 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-Unzip $docfxZip $docfx
+Unzip $rootFolder\$docfxZip $rootFolder\$docfx
 
 # clean api_ref
 Remove-Item "api_ref\*" -Force -recurse
