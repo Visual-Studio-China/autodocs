b8ab212eacc46648c169b75d880bc73051a86509
xuzho <xuzho@microsoft.com>
2016-09-30 10:31:52 +0800

    initial create a docs repo

---
+++ scripts/build.ps1
@@ -0,0 +1,49 @@
+$ErrorActionPreference = 'Stop'
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$rootFolder = Split-Path $scriptPath | Split-Path
+
+Push-Location $rootFolder
+
+$docfxZip = "docfx\docfx.zip"
+$docfx = "docfx\"
+$docfxExeUrl = "https://github.com/dotnet/docfx/releases/download/v2.1.0-cli-alpha/docfx.cli.zip"
+
+# download docfx from github.com
+Invoke-WebRequest $docfxExeUrl -OutFile $docfxZip
+
+# unzip docfx.zip
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip $docfxZip $docfx
+
+# clean api_ref
+Remove-Item "api_ref\*" -Force -recurse
+
+# run docfx metadata to generate YAML metadata
+& $docfx\docfx.exe metadata
+if($LASTEXITCODE -ne 0)
+{
+    Pop-Location
+    exit 1
+}
+
+# merge toc.yml if needed
+if(Test-Path scripts/mergeToc.js)
+{
+    & node scripts/mergeToc.js
+    if($LASTEXITCODE -ne 0)
+    {
+        Pop-Location
+        exit 1
+    }
+}
+
+
+Pop-Location
